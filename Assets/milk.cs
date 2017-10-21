146f820e2f0c9e674c7b9a5431489762df734c4b
Anthony Nguyen <anthonynguyen194@gmail.com>
2017-10-21 08:19:03 -0700

    Synced with upstream

---
+++ Assets/milk.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Milk : MonoBehaviour {
+    [SerializeField] private ParticleSystem particleSystem;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.transform.rotation.eulerAngles.y >= 90)
+        {
+
+        }
+	}
+}
